d08d4e7b141f5489ebdc00ae171ce7d0f08e36ad
Lorenzo Corbella <lorenzo.corbella@capgemini.com>
2019-01-02 17:02:13 +0100

    configurazione VSC

---
+++ src/components/Corbe.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      quote: 'I want to be single'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
