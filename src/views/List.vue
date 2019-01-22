7a4f0d66dfd7af8c40be916e5b5cb5c9cbd380fe
Lorenzo Corbella <lorenzo.corbella@capgemini.com>
2019-01-22 12:17:17 +0100

    Form validation con vuevalidate

--- src/views/List.vue
+++ src/views/List.vue
@@ -62,18 +62,18 @@
     </div>
     <!-- MODALE SALVA EDITA Studio-->
     <b-modal ref="myModalRef" :title="editmode? 'Edita studio':'Salva studio'">
-      <b-form>
+      <form>
         <b-form-group id="exampleInputGroup1" label="Titolo" label-for="formTitle">
           <b-form-input
             id="formTitle"
             type="text"
             name="titolo"
             v-model="form.title"
-            :state="!$v.form.title.$invalid"
-            aria-describedby="inputTitoloFeedback"
             placeholder="Inserire un titolo"
+            v-validate="'required'"
+            :class="{'is-invalid': submitted && errors.has('titolo') }"
           ></b-form-input>
-          <b-form-invalid-feedback id="inputTitoloFeedback">Il campo è richiesto</b-form-invalid-feedback>
+          <b-form-invalid-feedback>Il campo è richiesto</b-form-invalid-feedback>
         </b-form-group>
         <b-form-group id="exampleInputGroup2" label="Descrizione" label-for="exampleInput2">
           <b-form-textarea
@@ -81,13 +81,13 @@
             type="text"
             name="description"
             v-model="form.description"
-            :state="!$v.form.description.$invalid"
-            aria-describedby="inputDescrizioneFeedback"
             placeholder="Inserire una descrizione dello studio"
             :rows="3"
             :max-rows="6"
+            v-validate="'required'"
+            :class="{'is-invalid': submitted && errors.has('description') }"
           ></b-form-textarea>
-          <b-form-invalid-feedback id="inputDescrizioneFeedback">Il campo è richiesto</b-form-invalid-feedback>
+          <b-form-invalid-feedback>Il campo è richiesto</b-form-invalid-feedback>
         </b-form-group>
 
         <b-form-group id="exampleInputGroup3" label="Progress" label-for="exampleInput3">
@@ -105,22 +105,16 @@
             v-model="form.tipo"
             :options="optionsTipo"
             :select-size="4"
-            :state="!$v.form.tipo.$invalid"
-            aria-describedby="inputTipoFeedback"
+            v-validate="'required'"
+            :class="{'is-invalid': submitted && errors.has('description') }"
           />
-          <b-form-invalid-feedback id="inputTipoFeedback">Il campo è richiesto</b-form-invalid-feedback>
+          <b-form-invalid-feedback>Il campo è richiesto</b-form-invalid-feedback>
         </b-form-group>
-      </b-form>
+      </form>
       <div slot="modal-footer" class="w-100">
         <!-- <p class="float-left">Modal Footer Content</p> -->
-        <b-btn
-          size="md"
-          class="float-right"
-          variant="primary"
-          type="submit"
-          :disabled="$v.form.$invalid"
-          @click="onSubmit"
-        >Avanti</b-btn>
+        <b-btn size="md" class="float-right" variant="primary" type="submit" @click="onSubmit">Salva</b-btn>
+        <!-- :disabled="criteriaFulfill" -->
       </div>
     </b-modal>
   </div>
@@ -131,26 +125,10 @@ import Vue from 'vue';
 export const lista = [];
 
 // Form Validation
-import { validationMixin } from 'vuelidate';
-import { required, minLength } from 'vuelidate/lib/validators';
+import VeeValidate, { Validator } from 'vee-validate';
 
 export default {
   name: 'home',
-  mixins: [validationMixin],
-  validations: {
-    form: {
-      title: {
-        required
-      },
-      description: {
-        required,
-        minLength: minLength(12)
-      },
-      tipo: {
-        required
-      }
-    }
-  },
   data: function() {
     return {
       title: 'Studi',
@@ -163,6 +141,7 @@ export default {
         progress: 0,
         tipo: null
       },
+      submitted: false,
       optionsTipo: [
         { value: null, text: 'Selezionare una scala' },
         { value: 'maggiore', text: 'Maggiore' },
@@ -180,7 +159,7 @@ export default {
       //   title: 'Esempio 1',
       //   description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
       //   progress:0
-      //   data: ['c lydian']
+      //   data: []
       // }
     };
   },
@@ -228,41 +207,42 @@ export default {
     },
     onSubmit(evt) {
       evt.preventDefault();
-      if (this.form.title && this.form.description) {
-        if (this.editmode) {
-          var newItem = {
-            id: this.editedItem.id,
-            title: this.form.title,
-            description: this.form.description,
-            tipo: this.form.tipo,
-            progress: Number(this.form.progress),
-            data: this.editedItem.data || [],
-            date: this.editedItem.date
-          };
-          this.$set(this.items, this.editedItem.id, newItem);
-          this.$ls.set('lista', this.$data.items);
-          this.editmode = false;
-        } else {
-          var nextIndex = this.items.length; // si simula un id di partenza
-          var newItem = {
-            id: nextIndex,
-            title: this.form.title,
-            description: this.form.description,
-            tipo: this.form.tipo,
-            progress: Number(this.form.progress),
-            progress: this.form.progress,
-            date: new Date().toISOString(),
-            data: []
-          };
-          this.$set(this.items, nextIndex, newItem);
-          this.$ls.set('lista', this.$data.items);
+      this.submitted = true;
+      this.$validator.validate().then(valid => {
+        if (valid) {
+          if (this.editmode) {
+            var newItem = {
+              id: this.editedItem.id,
+              title: this.form.title,
+              description: this.form.description,
+              tipo: this.form.tipo,
+              progress: Number(this.form.progress),
+              data: this.editedItem.data || [],
+              date: this.editedItem.date
+            };
+            this.$set(this.items, this.editedItem.id, newItem);
+            this.$ls.set('lista', this.$data.items);
+            this.editmode = false;
+          } else {
+            var nextIndex = this.items.length; // si simula un id di partenza
+            var newItem = {
+              id: nextIndex,
+              title: this.form.title,
+              description: this.form.description,
+              tipo: this.form.tipo,
+              progress: Number(this.form.progress),
+              progress: this.form.progress,
+              date: new Date().toISOString(),
+              data: []
+            };
+            this.$set(this.items, nextIndex, newItem);
+            this.$ls.set('lista', this.$data.items);
+          }
+          // si chiude la modale
+          this.$refs.myModalRef.hide();
+          this.resetForm();
         }
-
-        this.$refs.myModalRef.hide();
-        this.resetForm();
-      } else {
-        evt.preventDefault();
-      }
+      });
     },
     resetForm() {
       this.form.title = '';
@@ -272,6 +252,9 @@ export default {
     }
   },
   computed: {
+    criteriaFulfill: function() {
+      return Boolean(this.errors.items != 0 || !this.form.title);
+    },
     filteredList() {
       if (this.items.length > 0) {
         if (this.listFilter == 'data') {
